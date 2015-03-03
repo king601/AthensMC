7cc2eec2331ac329086b861b643cde6677e2e048
Andrew <kaptoye@gmail.com>
2015-03-03 15:36:52 -0600

    Start building Casts authentication and Style play

--- app/controllers/casts_controller.rb
+++ app/controllers/casts_controller.rb
@@ -1,5 +1,20 @@
 class CastsController < ApplicationController
+	before_action :authenticate_user!, except: [:index]
+	before_action :check_admin_status, :only => [:new, :edit, :create, :destroy, :update]
+
 	def index
-		
 	end
+
+	private
+
+	def check_admin_status
+		authenticate_user!
+
+		if current_user.admin
+			return
+		else
+			redirect_to casts_path, notice: "You do not have permission to do that"
+		end
+	end
+
 end
