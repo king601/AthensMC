74d02fd4096248e960f88e62f8da3cdc1f48c255
Andrew <kaptoye@gmail.com>
2015-03-07 17:14:33 -0600

    Add Validations to Cast Submissions

--- app/models/cast.rb
+++ app/models/cast.rb
@@ -1,3 +1,8 @@
 class Cast < ActiveRecord::Base
+	validates :episode, presence: true, uniqueness: { case_sensitive: false }
+	validates :releasedate, presence: true
+	validates :description, presence: true
+	validates :embedcode, presence: true
+
 	belongs_to :user
 end
