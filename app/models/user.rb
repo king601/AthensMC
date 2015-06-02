94926b214be7adebaed7908c302907b38ddd6c2e
King601 <andrew@zerlex.net>
2015-06-01 23:38:51 -0500

    Add regex to validate username so they don't include spaces

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validates :username, format: {message: "can only contain letters, numbers or underscores", with: /\A[a-zA-Z0-9]+\z/ }
 
   validates :minecraft_uuid, presence: true, uniqueness: { case_sensitive: false }
 
   has_many :revisions
   has_many :casts
 end
+
