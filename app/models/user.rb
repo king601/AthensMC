eee7f3913c17571ba7bc287e614e8da2020ae639
King601 <andrew@zerlex.net>
2015-06-03 17:48:29 -0500

    Try more ways to do error validations...

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,22 +6,33 @@ class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { in: 2..32 }
   validates :username, format: {message: "can only contain letters, numbers, underscores or dashes.", with: /\A[A-Za-z0-9\-\_]+\z/ }
 
-  #validates :minecraft_uuid, presence: true, uniqueness: { case_sensitive: false }
+  #validates :minecraft_uuid, presence: true
+  validates :minecraft_uuid, presence: true, uniqueness: { case_sensitive: false }
 
   has_many :revisions
   has_many :casts
 
-  before_update :load_profile
+  #before_update :load_profile
 
-  def load_profile
-  	profile = MojangApi.get_profile_from_name(minecraft_uuid)
-  	assign_attributes(minecraft_uuid: profile.uuid)
+  #def load_profile
+  #	profile = MojangApi.get_profile_from_name(minecraft_uuid)
+  #	assign_attributes(minecraft_uuid: profile.uuid)
   	#if profile.uuid.nil?
   	#	raise "an error occured"
 	#end
 
-  end
+  #end
 
+
+  before_validation :set_uuid, on: :update
+
+  def set_uuid
+  	begin
+  	  self.minecraft_uuid = MojangApi.get_profile_from_name(minecraft_uuid).uuid	 	
+  	 rescue Exception => e
+  	 	 flash[:error] = "The internet broke"
+  	 end  
+  end
 
 end
 
