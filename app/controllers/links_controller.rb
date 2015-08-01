c1fca712b04c30868c4dd070ff972ad50f85b85a
King601 <andrew@zerlex.net>
2015-08-01 16:05:37 -0500

    Update links controller to catch the failure to redirect on nil params

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,25 +1,28 @@
 class LinksController < ApplicationController
 	before_action :authenticate_user!
-	
+
 	def minecraft
 		@user = current_user
 	end
 
 	def create
 		if current_user.update(user_params)
-			# Did the user enter in the UUID? IF so save it. 
+			# Did the user enter in the UUID? IF so save it.
 			if !current_user[:minecraft_uuid].nil?
 				redirect_to root_path, notice: "Successfully saved your Minecraft account!"
 			end
+			if current_user[:minecraft_uuid].nil?
+				redirect_to links_minecraft_path, alert: "You need to enter a Minecraft Username"
+			end
 		else
 			if !current_user.update(user_params)
-				redirect_to links_minecraft_path, alert: "An error occurred while looking up your Minecraft UUID, please try again!  
+				redirect_to links_minecraft_path, alert: "An error occurred while looking up your Minecraft UUID, please try again!
 				Make sure you double check your spelling.. In addition the Minecraft account cannot already be associated with an account."
 			end
 		end
 	end
 
-	
+
 	private
 		def user_params
 			params.require(:user).permit(:minecraft_uuid)
