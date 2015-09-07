a704fdd4a18d58b8cd434b5711836b6a8502df33
King601 <andrew@zerlex.net>
2015-09-06 23:25:58 -0500

    Downloads page should only be shown to users who have an account

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  before_action :authenticate_user!, :only => [:stats]
+  before_action :authenticate_user!, :only => [:stats, :downloads]
   def home
   end
 
