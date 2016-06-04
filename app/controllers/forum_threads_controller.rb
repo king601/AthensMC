849cbad190ea2fd5efb884f7d204c8a9d882b220
Andrew Fomera <andrew@zerlex.net>
2016-06-04 02:21:49 -0500

    Forum Threads should show posts up top with recent activity

--- app/controllers/forum_threads_controller.rb
+++ app/controllers/forum_threads_controller.rb
@@ -3,7 +3,7 @@ class ForumThreadsController < ApplicationController
   before_action :set_forum_thread, except: [:index, :new, :create]
 
   def index
-    @forum_threads = ForumThread.all.paginate(page: params[:page], per_page: 10)
+    @forum_threads = ForumThread.all.includes(:forum_posts).order("forum_posts.created_at DESC").paginate(page: params[:page], per_page: 10)
   end
 
   def new
