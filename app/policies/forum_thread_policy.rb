f71cc66232057d0766f7eaab2590b431f5dd3de5
Andrew Fomera <andrew@zerlex.net>
2016-10-25 20:36:00 -0500

    Add Pundit to forum threads (#13)
    * Refactor forumthreads controller and add pundit policy
    
    * Add some pundit policy checks to the pages
    
    * lint
    
    * lint
    
    * lint

---
+++ app/policies/forum_thread_policy.rb
@@ -0,0 +1,34 @@
+# ForumThreadPolicy
+class ForumThreadPolicy < ApplicationPolicy
+  attr_reader :user, :forum_thread
+
+  def initialize(user, forum_thread)
+    @user = user
+    @forum_thread = forum_thread
+  end
+
+  %w(index show new create).each do |action|
+    define_method("#{action}?") do
+      true
+    end
+  end
+
+  %w(edit update).each do |action|
+    define_method("#{action}?") do
+      owner?(forum_thread) || user.admin?
+    end
+  end
+
+  %w(sticky unsticky destroy).each do |action|
+    define_method("#{action}?") do
+      user.admin?
+    end
+  end
+
+  # ForumThreadPolicy Scope
+  class Scope < Scope
+    def resolve
+      scope
+    end
+  end
+end
