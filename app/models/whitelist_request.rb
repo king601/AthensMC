91dbb3b54a348c20a9c3690d70c1cabb720dd92c
Andrew Fomera <andrew@zerlex.net>
2016-11-25 19:39:52 -0600

    cleanup linting

--- app/models/whitelist_request.rb
+++ app/models/whitelist_request.rb
@@ -4,13 +4,19 @@ class WhitelistRequest < ApplicationRecord
 
   after_create_commit :create_slack_notification unless Rails.env.development?
 
-  validates :agree_rules, presence: { message: 'You need to agree to the Community Rules' }
+  validates :agree_rules, presence: {
+    message: 'You need to agree to the Community Rules'
+  }
 
-  scope :status, -> (status) { where(status: status) } # WhitelistRequest.status("pending") || WhitelistRequest.status("approved")
+  scope :status, -> (status) { where(status: status) }
   scope :agree, -> (agree) { where(agree_rules: agree) }
 
   # All whitelist requests in the current month
-  scope :current_month, -> { where('created_at  BETWEEN ? AND ?', Time.zone.now.beginning_of_month, Time.zone.now.end_of_month) }
+  scope :current_month, -> {
+    where('created_at  BETWEEN ? AND ?',
+          Time.zone.now.beginning_of_month,
+          Time.zone.now.end_of_month)
+  }
 
   scope :last_month, -> { where('created_at  BETWEEN ? AND ?', 1.month.ago,
     Time.zone.now.beginning_of_month) }
