530010de8a3d34f8d2cf260253d1f5b99c280d1b
Andrew Fomera <andrew@zerlex.net>
2016-12-02 22:55:04 -0600

    Show all WhitelistRequests for charts

--- app/controllers/admin/whitelist_requests_controller.rb
+++ app/controllers/admin/whitelist_requests_controller.rb
@@ -10,9 +10,9 @@ class Admin::WhitelistRequestsController < ApplicationController
   end
 
   def charts
-    @whitelist_requests_chart = WhitelistRequest.current_month.group_by_day(:created_at).count
-    @whitelist_requests_by_status = WhitelistRequest.current_month.group(:status).group_by_week(:created_at).count
-    @last_month_chart = WhitelistRequest.last_month.group_by_day(:created_at).count
+    @whitelist_requests_chart = WhitelistRequest.all.group_by_day(:created_at).count
+    @whitelist_requests_by_status = WhitelistRequest.all.group(:status).group_by_week(:created_at).count
+    @last_month_chart = WhitelistRequest.all.group_by_day(:created_at).count
   end
 
   def pending
