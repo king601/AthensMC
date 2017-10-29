a5158fe8e75893365deff169edc1e391bd3a6182
Andrew Fomera <andrew@zerlex.net>
2017-10-28 21:37:43 -0500

    Cleanup SlackNotifierJob

--- app/jobs/slack_notifier_job.rb
+++ app/jobs/slack_notifier_job.rb
@@ -3,22 +3,53 @@ require 'slack-notifier'
 # SlackNotifierJob
 class SlackNotifierJob < ApplicationJob
   queue_as :default
+  attr_accessor :whitelist_request, :notifier
 
   def perform(whitelist_request)
+    @whitelist_request = whitelist_request
     # Do something later
-    notifier = Slack::Notifier.new Rails.application.secrets.slack_token
-    notifier.username = 'AthensMC Whitelist System'
-    message = "<!channel> #{whitelist_request.user.username} just applied for the community\n[View Whitelist Requests](https://athensmc.com/admin/whitelist-requests)"
     Slack::Notifier::LinkFormatter.format(message)
-    more_user_info = {
-      fallback: "AthensMC Username: #{whitelist_request.user.username}\nMinecraft UUID: #{whitelist_request.user.minecraft_uuid}",
-      text: "AthensMC Username: #{whitelist_request.user.username}\nMinecraft UUID: #{whitelist_request.user.minecraft_uuid}\nReferred by: #{whitelist_request.referred_by}",
-      color: 'good'
-    }
     notifier.ping(
       message,
       icon_emoji: ':sparkles:',
       attachments: [more_user_info]
     )
+  end
+
+  private
+
+  def notifier
+    @notifier ||= Slack::Notifier.new(
+      Rails.application.secrets.slack_token,
+      username: 'AthensMC Whitelist System'
+    )
+  end
+
+  def message
+    "<!here> New Whitelist Request!\n"\
+    "#{whitelist_request.user.username} just applied for the community"\
+    "\n#{whitelist_requests_link}"
+  end
+
+  def more_user_info
+    {
+      fallback: fallback_message,
+      text: text,
+      color: 'good'
+    }
+  end
+
+  def text
+    "AthensMC Username: #{whitelist_request.user.username}\n"\
+    "Minecraft UUID: #{whitelist_request.user.minecraft_uuid}\n"\
+    "Referred by: #{whitelist_request.referred_by}"
+  end
+
+  def fallback_message
+    "View whitelist requests on the site"
+  end
+
+  def whitelist_requests_link
+    "[View Whitelist Requests](https://athensmc.com/admin/whitelist-requests)"
   end
 end
