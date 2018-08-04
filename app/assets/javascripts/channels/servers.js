cb2b3d12b8671d55a3b891ab06054bf6f1fd8e19
Andrew Fomera <andrew@zerlex.net>
2018-08-03 23:48:39 -0500

    remove console logs

--- app/assets/javascripts/channels/servers.js
+++ app/assets/javascripts/channels/servers.js
@@ -6,17 +6,17 @@ App.server = App.cable.subscriptions.create(
   {
     connected: function() {
       // Called when the subscription is ready for use on the server
-      console.log('connected');
+      // console.log('connected');
     },
 
     disconnected: function() {
       // Called when the subscription has been terminated by the server
-      console.log('disconnected');
+      // console.log('disconnected');
     },
 
     received: function(data) {
       // Called when there's incoming data on the websocket for this channel
-      console.log(data);
+      // console.log(data);
       if (data.server_id == $('[data-server]').data('server')) {
         $("[data-behavior='rcon-response']").append(data.html);
       } else {
