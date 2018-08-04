983e5dd287ae7e3b9c67186be67b20cac6996ae8
Andrew Fomera <andrew@zerlex.net>
2018-08-03 23:33:03 -0500

    import ruby class for rcon and make it work and send commands gs

--- app/services/rcon/command_sender.rb
+++ app/services/rcon/command_sender.rb
@@ -1,3 +1,5 @@
+require 'rcon/rcon'
+
 module Rcon
   class CommandSender
     attr_accessor :server, :command
@@ -7,8 +9,20 @@ module Rcon
     end
 
     def send!
-      Rails.logger.info("Connect to RCON for #{server.name}")
-      Rails.logger.info("Running command: #{command}")
+      rcon = ::RCON::Minecraft.new(@server.ip, @server.rcon_port)
+      rcon.auth(@server.rcon_password)
+      Rails.logger.info(rcon)
+      command_output = rcon.command(@command)
+
+      if command_output
+        ActionCable.server.broadcast(
+          "servers_channel",
+          server_id: @server.id,
+          command_recieved: "#{@command}",
+          html: "<div>#{command_output}</div>"
+        )
+      end
+      rcon.disconnect
     end
   end
 end
