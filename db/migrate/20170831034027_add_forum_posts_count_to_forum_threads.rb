dad847890f9f017f6d382b5590e92be594ae312e
Andrew Fomera <andrew@zerlex.net>
2019-01-08 22:01:33 -0600

    fix migration that was broke

--- db/migrate/20170831034027_add_forum_posts_count_to_forum_threads.rb
+++ db/migrate/20170831034027_add_forum_posts_count_to_forum_threads.rb
@@ -2,8 +2,6 @@ class AddForumPostsCountToForumThreads < ActiveRecord::Migration[5.1]
   def up
     add_column :forum_threads, :forum_posts_count, :integer
     ForumThread.find_each { |thread| ForumThread.reset_counters(thread.id, :forum_posts) }
-
-    ForumThread.reindex
   end
 
   def down
